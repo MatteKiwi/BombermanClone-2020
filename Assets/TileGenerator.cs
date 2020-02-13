3fb113260e978527cc2ab8d0e5798f738ecc38e0
MatteKiwi <55237242+MatteKiwi@users.noreply.github.com>
2020-02-13 14:54:20 +0100

    yo

---
+++ Assets/TileGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject tile;
+    [SerializeField]
+    int width = 7;
+    [SerializeField]
+    int height = 7;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        Vector3 offset = new Vector3(width / 2f - 0.5f, height / 2f - 0.5f);
+
+
+        for (int y = 0; y < height; y++)
+        {
+
+            for (int x = 0; x < width; x++)
+            {
+                MakeTile(offset, y, x);
+            }
+
+        }
+
+
+
+    }
+
+    protected virtual void MakeTile(Vector3 offset, int y, int x)
+    {
+        GameObject newTile = Instantiate(tile, new Vector3(x, y, transform.position.z) - offset, Quaternion.identity);
+        newTile.name = tile.name + " " + x + "," + y;
+        newTile.transform.parent = transform;
+    }
+}
---
+++ Assets/WallGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallGenerator : TileGenerator
+{
+    protected override void MakeTile(Vector3 offset, int y, int x)
+    {
+        if (x == 0)
+        {
+            base.MakeTile(offset, y, x);
+        }
+        else if (y == 0)
+        {
+            base.MakeTile(offset, y, x);
+        }
+
+
+    }
+}
